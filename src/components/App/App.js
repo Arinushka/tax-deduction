c49cb0a31ea8c7613a1d327e997432b10b9755da
Arina Mykhova <arinabagira@mail.ru>
2021-08-10 22:03:45 +0300

    feat: сделать компонент main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Main } from './../Main/Main';
 
 export const App = () => {
   return (
     <div className="App">
-     
+     <Main/>
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import './Main.css';
+
+export const Main = () => {
+	return (
+		<main className="main">
+			<button className="main__button" type="button">Налоговый вычет</button>
+		</main>
+	);
+}
